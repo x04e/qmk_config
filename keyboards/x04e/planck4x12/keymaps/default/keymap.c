6a5e8543914136ed2a76aac0857de5d43f882dac
Liam Hilliard <liam.h.-@hotmail.com>
2022-01-19 15:48:13 +0000

    Add OneShot and custom toggleable GUI key
    OneShot modifiers work well for Ctrl Alt and Shift, but when using
    LGUI as the i3 modifer I want to toggle it on and off.
    
    Enabled console logging. Removed unused default configuration
    from rules.mk and config.h

--- keyboards/x04e/planck4x12/keymaps/default/keymap.c
+++ keyboards/x04e/planck4x12/keymaps/default/keymap.c
@@ -1,14 +1,24 @@
 #include QMK_KEYBOARD_H
 
 #define _QWERTY     0
-#define _COLEMAK    1
-#define _DVORAK     2
-#define _NAV        3
-#define _LOWER      4
-#define _RAISE      5
-#define _ADJUST     6
+#define _COLEMAK_DH 1
+#define _LOWER      3
+#define _RAISE      4
+#define _ADJUST     5
 
 #define QWERTY      DF(_QWERTY)
+#define COLMDH      DF(_COLEMAK_DH)
+#define LOWER       TO(_LOWER)
+#define RAISE       TO(_RAISE)
+#define ADJUST      TO(_ADJUST)
+
+enum custom_keycodes { MY_GUI };
+
+#define _CTL        OSM(MOD_LCTL)
+#define _SFT        OSM(MOD_LSFT)
+#define _ALT        OSM(MOD_LALT)
+#define _GUI        MY_GUI
+
 
 /*
     Note
@@ -22,13 +32,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT( \
     //  TAB         Q           W           E           R           T           Y           U           I           O           P           '@
-        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       MT(MOD_LSFT,KC_T),       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_QUOT,    \
+        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_QUOT,    \
     //  ESC         A           S           D           F           G           H           J           K           L           ;:          BACKSPACE
         KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_BSPC,    \
     //  LSHIFT      Z           X           C           V           B           N           M           ,<          .>          /?          ENTER
-        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,     \
+        _SFT,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,     \
     //  LCTRL       LALT                    LGUI        LOWER       SPACE       LSHIFT      RAISE       MEDIA PLAY  VOL +       VOL -       DELETE
-        KC_LCTL,    KC_LALT,    XXXXXXX,    KC_LGUI,    MO(_LOWER), KC_SPC,     KC_LSFT,    MO(_RAISE), KC_MPLY,    KC__VOLUP,  KC__VOLDOWN,KC_DEL  \
+        _CTL,       _ALT,       XXXXXXX,    _GUI,       MO(_LOWER), KC_SPC,     _SFT,    MO(_RAISE), KC_MPLY,    KC__VOLUP,  KC__VOLDOWN,KC_DEL  \
     ),
 
     [_RAISE] = LAYOUT( \
@@ -65,3 +75,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 
 };
+
+
+// Allow GUI key to be toggled or held
+#define GUI_DELAY 500
+bool gui_active = false;
+uint32_t gui_timer = 0;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MY_GUI:
+            if(record->event.pressed){
+                gui_active = !gui_active;
+                if(gui_active) {
+                    println("process_record_user: gui_active = true");
+                    register_code(KC_LGUI);
+                } else {
+                    println("process_record_user: gui_active = false");
+                    unregister_code(KC_LGUI);
+                }
+            } else {
+                if(timer_elapsed32(gui_timer) > GUI_DELAY){
+                    gui_active = false;
+                    unregister_code(KC_LGUI);
+                }
+            }
+            gui_timer = timer_read32();
+            break;
+    }
+    return true;
+}
+
