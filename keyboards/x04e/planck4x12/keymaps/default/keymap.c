dd652cb2ebf769157496a60551706564d408a240
Liam Hilliard <liam.h.-@hotmail.com>
2022-01-20 02:50:37 +0000

    Add shortcut codes. Update keymap
    Added a shortcut.h file with 3-5 character codes for keys. This makes
    formatting and changing the keymap easier.
    
    Added Colemak and Halmak layers.
    
    Changed the GUI key from custom hold to just the normal KC_LGUI key. The
    custom hold was more annoying than helpful.
    
    Changed layer keys to toggles. Added code to swap back to whatever the
    current default layer is instead of having to manually re-select
    QWERTY, Colemak, Halmak, etc.

--- keyboards/x04e/planck4x12/keymaps/default/keymap.c
+++ keyboards/x04e/planck4x12/keymaps/default/keymap.c
@@ -1,106 +1,126 @@
 #include QMK_KEYBOARD_H
+#include "print.h"
+#include "shortcuts.h"
 
-#define _QWERTY     0
-#define _COLEMAK_DH 1
-#define _LOWER      3
-#define _RAISE      4
-#define _ADJUST     5
+#define LAYER_QWERTY     0
+#define LAYER_COLEMAK_DH 1
+#define LAYER_HALMAK     2
 
-#define QWERTY      DF(_QWERTY)
-#define COLMDH      DF(_COLEMAK_DH)
-#define LOWER       TO(_LOWER)
-#define RAISE       TO(_RAISE)
-#define ADJUST      TO(_ADJUST)
+#define LAYER_LOWER      10
+#define LAYER_RAISE      11
+#define LAYER_ADJUST     12
 
-enum custom_keycodes { MY_GUI };
+#define QWERTY      DF(LAYER_QWERTY)
+#define COLMAK      DF(LAYER_COLEMAK_DH)
+#define HALMAK      DF(LAYER_HALMAK)
+#define LOWER       MO(LAYER_LOWER)
+#define RAISE       MO(LAYER_RAISE)
+#define ADJUST      MO(LAYER_ADJUST)
 
-#define _CTL        OSM(MOD_LCTL)
-#define _SFT        OSM(MOD_LSFT)
+enum custom_keycodes { TODEF };
+
+#undef _CTRL
+#undef _SHFT
+#undef _ALT
+#undef _GUI
+
+#define _CTRL       OSM(MOD_LCTL)
+#define _SHFT       OSM(MOD_LSFT)
 #define _ALT        OSM(MOD_LALT)
-#define _GUI        MY_GUI
-
-
-/*
-    Note
-    KC_NUHS         #
-    Shift+KC_NUHS   ~
-    KC_NUBS         \
-    Shift+KC_NUBS   |
-*/
+#define _GUI        KC_LGUI
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_QWERTY] = LAYOUT( \
-    //  TAB         Q           W           E           R           T           Y           U           I           O           P           '@
-        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_QUOT,    \
-    //  ESC         A           S           D           F           G           H           J           K           L           ;:          BACKSPACE
-        KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_BSPC,    \
-    //  LSHIFT      Z           X           C           V           B           N           M           ,<          .>          /?          ENTER
-        _SFT,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,     \
-    //  LCTRL       LALT                    LGUI        LOWER       SPACE       LSHIFT      RAISE       MEDIA PLAY  VOL +       VOL -       DELETE
-        _CTL,       _ALT,       XXXXXXX,    _GUI,       MO(_LOWER), KC_SPC,     _SFT,    MO(_RAISE), KC_MPLY,    KC__VOLUP,  KC__VOLDOWN,KC_DEL  \
+    [LAYER_QWERTY] = LAYOUT( \
+    /*
+        TAB     Q       W       E       R       T       Y       U       I       O       P       '@
+        ESC     A       S       D       F       G       H       J       K       L       ;:      BCSP
+        SHIFT   Z       X       C       V       B       N       M       ,<      .>      /?      ENT
+        CTRL    ALT     xxx     GUI     LOWER   SPC     SHFT    RAISE   PLAY    VOL+    VOL-    DEL
+        |       |       |       |       |       |       |       |       |       |       |       |       | */
+        _TAB,   _Q,     _W,     _E,     _R,     _T,     _Y,     _U,     _I,     _O,     _P,     _SQT,   \
+        _ESC,   _A,     _S,     _D,     _F,     _G,     _H,     _J,     _K,     _L,     _SCN,   _BSPC,  \
+        _SHFT,  _Z,     _X,     _C,     _V,     _B,     _N,     _M,     _COM,   _DOT,   _SL,    _ENT,   \
+        _CTRL,  _ALT,   xxx,    _GUI,   LOWER,  _SPC,   _SHFT,  RAISE,  _V_P,   _V_VU,  _V_VD,  _DEL    \
     ),
 
-    [_RAISE] = LAYOUT( \
-    //                          7           8           9
-        _______,    XXXXXXX,    KC_7,       KC_8,       KC_9,       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    //              0           4           5           6                                   LEFT ARR    DOWN ARR    UP ARR      RIGHT ARR
-        _______,    KC_0,       KC_4,       KC_5,       KC_6,       XXXXXXX,    XXXXXXX,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    \
-    //                          1           2           3                                   HOME        PAGE DOWN   PAGE UP     END
-        _______,    XXXXXXX,    KC_1,       KC_2,       KC_3,       XXXXXXX,    XXXXXXX,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    \
-    //                                                  ADJUST                              RAISE
-        _______,    _______,    _______,    _______,    MO(_ADJUST),_______,    _______,    KC_TRNS,    _______,    _______,    _______,    _______     \
+    [LAYER_COLEMAK_DH] = LAYOUT( \
+    /*
+        TAB     Q       W       F       P       B       J       L       U       Y       ;:       '@
+        ESC     A       R       S       T       G       M       N       E       I       O       BCSP
+        SHIFT   Z       X       C       D       V       K       H       ,<      .>      /?      ENT
+        CTRL    ALT     xxx     GUI     LOWER   SPC     SHFT    RAISE   PLAY    VOL+    VOL-    DEL
+        |       |       |       |       |       |       |       |       |       |       |       |       | */
+        _TAB,   _Q,     _W,     _F,     _P,     _B,     _J,     _L,     _U,     _Y,     _SCN,   _SQT,   \
+        _ESC,   _A,     _R,     _S,     _T,     _G,     _M,     _N,     _E,     _I,     _O,     _BSPC,  \
+        _SHFT,  _Z,     _X,     _C,     _D,     _V,     _K,     _H,     _COM,   _DOT,   _SL,    _ENT,   \
+        _CTRL,  _ALT,   xxx,    _GUI,   LOWER,  _SPC,   _SHFT,  RAISE,  _V_P,   _V_VU,  _V_VD,  _DEL    \
     ),
 
-    [_LOWER] = LAYOUT( \
-    //  £           `           |           /           _           [           ]           +           $           &           <           >
-        LSFT(KC_3), KC_GRV,     LSFT(KC_NUBS),KC_SLSH,  LSFT(KC_MINS),KC_LBRC,  KC_RBRC,    LSFT(KC_EQL),LSFT(KC_4),LSFT(KC_7), LSFT(KC_COMM),LSFT(KC_DOT),\
-    //  %           ?           ;           '           -           (           )           .           "           ,           !
-        LSFT(KC_5), LSFT(KC_SLSH),KC_SCLN,  KC_QUOT,    KC_MINS,    LSFT(KC_9), LSFT(KC_0), KC_DOT,     LSFT(KC_2), KC_COMM,    LSFT(KC_1),   _______,  \
-    //  €           ^           :           \           ~           {           }           =           *           #           @
-        RALT(KC_4), LSFT(KC_6), LSFT(KC_SCLN),KC_NUBS,  LSFT(KC_NUHS),KC_LCBR,  KC_RCBR,    KC_EQL,     LSFT(KC_8), KC_NUHS,    LSFT(KC_QUOT),_______,  \
-    //                                                  LOWER                               ADJUST
-        _______,    _______,    _______,    _______,    KC_TRNS,    _______,    _______,    MO(_ADJUST),_______,    _______,    _______,    _______     \
+    [LAYER_HALMAK] = LAYOUT( \
+    /*
+        TAB     W       L       R       B       Z       ;:      Q       U       D       J       '@
+        ESC     S       H       N       T       ,<      .>      A       E       O       I       BCSP
+        SHIFT   F       M       V       C       /?      G       P       X       K       Y       ENT
+        CTRL    ALT     xxx     GUI     LOWER   SPC     SHFT    RAISE   PLAY    VOL+    VOL-    DEL
+        |       |       |       |       |       |       |       |       |       |       |       |       | */
+        _TAB,   _W,     _L,     _R,     _B,     _Z,     _SCN,   _Q,     _U,     _D,     _J,     _SQT,   \
+        _ESC,   _S,     _H,     _N,     _T,     _COM,   _DOT,   _A,     _E,     _O,     _I,     _BSPC,  \
+        _SHFT,  _F,     _M,     _V,     _C,     _SL,    _G,     _P,     _X,     _K,     _Y,     _ENT,   \
+        _CTRL,  _ALT,   xxx,    _GUI,   LOWER,  _SPC,   _SHFT,  RAISE,  _V_P,   _V_VU,  _V_VD,  _DEL    \
     ),
 
-    [_ADJUST] = LAYOUT( \
-    //  F1          F2          F3          F4          F5          F6          F7          F8          F9          F10         F11         F12
-        KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     \
-    //  CAPSLOCK    MS FAST     MS SLOW     MS RCLICK   MS LCLICK                           MS LEFT     MS DOWN     MS UP       MS RIGHT
-        KC_CAPS,    KC_ACL2,    KC_ACL0,    KC_BTN2,    KC_BTN1,    XXXXXXX,    XXXXXXX,    KC_MS_L,    KC_MS_D,    KC_MS_U,    KC_MS_R,    XXXXXXX,    \
-    //                                                                                                  MS WHL DN   MS WHL UP
-        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_WH_D,    KC_WH_U,    XXXXXXX,    XXXXXXX,    \
-    //                                                                                                  PLAY        VOL+        VOL-        #RESET
-        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_TRNS,    XXXXXXX,    XXXXXXX,    KC_TRNS,    KC_MPLY,    KC__VOLUP,  KC__VOLDOWN,RESET       \
+    [LAYER_RAISE] = LAYOUT( \
+    /*  ___     xxx     7       8       9       xxx     xxx     xxx     xxx     xxx     xxx     xxx
+        ___     0       4       5       6       xxx     xxx     LEFT    DOWN    UP      RIGHT   xxx
+        ___     xxx     1       2       3       xxx     xxx     HOME    PGDN    PGUP    END     xxx
+        ___     xxx     xxx     xxx     ADJUST  ___     ___     TODEF   ___     ___     ___     ___
+        |       |       |       |       |       |       |       |       |       |       |       |       | */
+        ___,    xxx,    _7,     _8,     _9,     xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    \
+        ___,    _0,     _4,     _5,     _6,     xxx,    xxx,    _AR_L,  _AR_D,  _AR_U,  _AR_R,  ___,    \
+        ___,    xxx,    _1,     _2,     _3,     xxx,    xxx,    _HOME,  _PG_D,  _PG_U,  _END,   ___,    \
+        ___,    ___,    ___,    ___,    ADJUST, ___,    ___,    TODEF,  ___,    ___,    ___,    ___     \
+    ),
+
+    [LAYER_LOWER] = LAYOUT( \
+    /*  £       `       |       /       _       [       ]       +       $       &       <       >
+        %       ?       ;       '       -       (       )       .       "       ,       !       ___
+        €       ^       :       \       ~       {       }       =       *       #       @       ___
+        ___     ___     ___     ___     TODEF   ___     ___     ADJUST  ___     ___     ___     ___
+        |       |       |       |       |       |       |       |       |       |       |       |       | */
+        _GBP,   _BTK,   _PIP,   _SL,    _UND,   _LSB,  _RSB,    _PLS,   _USD,   _AMP,   _LAB,   _RAB,   \
+        _CEN,   _QS,    _SCN,   _SQT,   _MIN,   _LPR,  _RPR,    _DOT,   _DQT,   _COM,   _EX,    ___,    \
+        _EUR,   _CRT,   _CN,    _BSL,   _TLD,   _LCB,  _RCB,    _EQ,    _AST,   _HSH,   _AT,    ___,    \
+        ___,    ___,    ___,    ___,    TODEF,  ___,    ___,    ADJUST, ___,    ___,    ___,    ___     \
+    ),
+
+    [LAYER_ADJUST] = LAYOUT( \
+    /*  F1      F2      F3      F4      F5      F6      F7      F8      F9      F10     F11     F12
+        CAPS    FAST    SLOW    RCLK    LCLK    xxx     xxx     MLEFT   MDOWN   MUP     MRIGHT  xxx
+        xxx     xxx     xxx     xxx     xxx     xxx     xxx     xxx     WHDOWN  WHUP    xxx     xxx
+        xxx     QWERTY  COLMAK  HALMAK  TODEF   xxx     xxx     TODEF   PLAY    VOL+    VOL-    RESET
+        |       |       |       |       |       |       |       |       |       |       |       |       | */
+        _F1,    _F2,    _F3,    _F4,    _F5,    _F6,    _F7,    _F8,    _F9,    _F10,   _F11,   _F12,   \
+        _CAPS,  _M_A2,  _M_A0,  _M_B2,  _M_B1,  xxx,    xxx,    _M_L,   _M_D,   _M_U,   _M_R,   xxx,    \
+        xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    _M_WD,  _M_WU,  xxx,    xxx,    \
+        xxx,    QWERTY, COLMAK, HALMAK, TODEF,  xxx,    xxx,    TODEF,  _V_P,   _V_VU,  _V_VD,  RESET   \
     )
 
 };
 
+void keyboard_post_init_user(void) { debug_enable=true; }
 
-// Allow GUI key to be toggled or held
-#define GUI_DELAY 500
-bool gui_active = false;
-uint32_t gui_timer = 0;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case MY_GUI:
+        /* Reset the layer to default. Will work with any number of default layers */
+        case TODEF:
             if(record->event.pressed){
-                gui_active = !gui_active;
-                if(gui_active) {
-                    println("process_record_user: gui_active = true");
-                    register_code(KC_LGUI);
-                } else {
-                    println("process_record_user: gui_active = false");
-                    unregister_code(KC_LGUI);
-                }
-            } else {
-                if(timer_elapsed32(gui_timer) > GUI_DELAY){
-                    gui_active = false;
-                    unregister_code(KC_LGUI);
-                }
+                /* default_layer_state is 0 until DF() is used to change it,
+                 * then it is 2*<layer> (no idea why). We shift it to the
+                 * right (divide by 2) to get the layer code
+                 */
+                layer_move(default_layer_state >> 1);
             }
-            gui_timer = timer_read32();
-            break;
     }
     return true;
 }
---
+++ shortcuts.h
@@ -0,0 +1,147 @@
+// Custom codes for QMK
+// Each begins with an underscore
+// and is no more than 5 character long
+
+// QMK codes
+#define ___        KC_TRNS
+#define xxx        KC_NO
+
+
+// Control keys
+#define _ESC        KC_ESCAPE
+#define _TAB        KC_TAB
+#define _CTRL       KC_LCTRL
+#define _SHFT       KC_LSFT
+#define _ALT        KC_LALT
+#define _GUI        KC_LGUI
+#define _DEL        KC_DELETE
+#define _ENT        KC_ENTER
+#define _BSPC       KC_BACKSPACE
+#define _SPC        KC_SPACE
+#define _AR_L       KC_LEFT
+#define _AR_D       KC_DOWN
+#define _AR_U       KC_UP
+#define _AR_R       KC_RIGHT
+#define _HOME       KC_HOME
+#define _PG_D       KC_PAGE_DOWN
+#define _PG_U       KC_PAGE_UP
+#define _END        KC_END
+#define _INS        KC_INSERT
+#define _CAPS       KC_CAPS
+
+// Function keys
+#define _F1         KC_F1
+#define _F2         KC_F2
+#define _F3         KC_F3
+#define _F4         KC_F4
+#define _F5         KC_F5
+#define _F6         KC_F6
+#define _F7         KC_F7
+#define _F8         KC_F8
+#define _F9         KC_F9
+#define _F10        KC_F10
+#define _F11        KC_F11
+#define _F12        KC_F12
+
+// Alphabet
+#define _A          KC_A
+#define _B          KC_B
+#define _C          KC_C
+#define _D          KC_D
+#define _E          KC_E
+#define _F          KC_F
+#define _G          KC_G
+#define _H          KC_H
+#define _I          KC_I
+#define _J          KC_J
+#define _K          KC_K
+#define _L          KC_L
+#define _M          KC_M
+#define _N          KC_N
+#define _O          KC_O
+#define _P          KC_P
+#define _Q          KC_Q
+#define _R          KC_R
+#define _S          KC_S
+#define _T          KC_T
+#define _U          KC_U
+#define _V          KC_V
+#define _W          KC_W
+#define _X          KC_X
+#define _Y          KC_Y
+#define _Z          KC_Z
+
+// Numbers
+#define _1          KC_1
+#define _2          KC_2
+#define _3          KC_3
+#define _4          KC_4
+#define _5          KC_5
+#define _6          KC_6
+#define _7          KC_7
+#define _8          KC_8
+#define _9          KC_9
+#define _0          KC_0
+
+// Symbols
+#define _SQT        KC_QUOT //          ' @
+#define _SCN        KC_SCLN //          ; :
+#define _COM        KC_COMM //          , <
+#define _DOT        KC_DOT  //          . >
+#define _SL         KC_SLSH //          / ?
+#define _GBP        LSFT(KC_3) //       £
+#define _CEN        LSFT(KC_5) //       %
+#define _EUR        RALT(KC_4) //       €
+#define _BTK        KC_GRV //           `
+#define _QS         LSFT(KC_SLSH) //    ?
+#define _CRT        LSFT(KC_6) //       ^
+#define _PIP        LSFT(KC_NUBS) //    |
+#define _CN         LSFT(KC_SCLN) //    :
+#define _UND        LSFT(KC_MINS) //    _
+#define _MIN        KC_MINS //          -
+#define _TLD        LSFT(KC_NUHS) //    ~
+#define _LSB        KC_LBRC //          [
+#define _RSB        KC_RBRC //          ]
+#define _LCB        KC_LCBR //          {
+#define _RCB        KC_RCBR //          }
+#define _LPR        LSFT(KC_9) //       (
+#define _RPR        LSFT(KC_0) //       )
+#define _LAB        LSFT(KC_COMM) //    <
+#define _RAB        LSFT(KC_DOT) //     >
+#define _PLS        LSFT(KC_EQL) //     +
+#define _EQ         KC_EQL //           =
+#define _USD        LSFT(KC_4) //       $
+#define _AMP        LSFT(KC_7) //       &
+#define _DQT        LSFT(KC_2) //       "
+#define _AST        LSFT(KC_8) //       *
+#define _HSH        KC_NUHS //          #
+#define _AT         LSFT(KC_QUOT) //    @
+#define _EX         LSFT(KC_1) //       !
+#define _BSL        KC_NUBS /*          \
+*/
+
+// Media keys
+#define _V_P        KC_MEDIA_PLAY_PAUSE
+#define _V_VD       KC_AUDIO_VOL_DOWN
+#define _V_VU       KC_AUDIO_VOL_UP
+#define _V_NT       KC_MEDIA_NEXT_TRACK
+#define _V_PT       KC_MEDIA_PREV_TRACK
+
+// Mouse keys
+#define _M_L        KC_MS_LEFT
+#define _M_D        KC_MS_DOWN
+#define _M_U        KC_MS_UP
+#define _M_R        KC_MS_RIGHT
+#define _M_B1       KC_MS_BTN1
+#define _M_B2       KC_MS_BTN2
+#define _M_B3       KC_MS_BTN3
+#define _M_B4       KC_MS_BTN4
+#define _M_B5       KC_MS_BTN5
+#define _M_WL       KC_MS_WH_LEFT
+#define _M_WD       KC_MS_WH_DOWN
+#define _M_WU       KC_MS_WH_UP
+#define _M_WR       KC_MS_WH_RIGHT
+#define _M_A0       KC_ACL0
+#define _M_A1       KC_ACL1
+#define _M_A2       KC_ACL2
+
