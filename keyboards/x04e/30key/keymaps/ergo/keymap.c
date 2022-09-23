cb10400b69a52ac01753c45af7b4be3c65a3330b
Liam Hilliard <liam.h.-@hotmail.com>
2022-09-23 19:33:16 +0100

    Remove hold keys

--- keyboards/x04e/30key/keymaps/ergo/keymap.c
+++ keyboards/x04e/30key/keymaps/ergo/keymap.c
@@ -32,33 +32,29 @@ enum layers {
 /* Other mods */
 #define _UPPR      LT(0, KC_NO)
 #define _COM_DOT   LT(0, _DOT)
-#define _R_TAB     LT(0, _R)
-#define _V_ESC     LT(0, _V)
-#define _U_DEL     LT(0, _U)
-#define _M_ENT     LT(0, _M)
-#define _C_BSPC    LT(0, _C)
-#define _SPC_BSPC  LT(0, _SPC)
+#define _V_BSPC    LT(0, _V)
+#define _M_BSPC    LT(0, _M)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_QWERTY] = LAYOUT( \
      /* ────────── ────────── ────────── ────────── ──────────|────────── ────────── ────────── ────────── ────────── */
-        _Q_LGUI,   _W,        _E,        _R_TAB,    _T,        _Y,        _U_DEL,    _I,        _O,        _P_RGUI,   \
+        _Q_LGUI,   _W,        _E,        _R,        _T,        _Y,        _U,        _I,        _O,        _P_RGUI,   \
         _A_LCTL,   _S_LSFT,   _D_LSYM,   _F_LARR,   _G,        _H,        _J_RNUM,   _K_RSYM,   _L_RSFT,   _SCN_RCTL, \
-        _Z_LALT,    _X,       _C_BSPC,   _V_ESC,    _B,        _N,        _M_ENT,    _SPC_BSPC, _COM_DOT,  _SL_RALT   \
+        _Z_LALT,    _X,       _C,        _V_BSPC,   _B,        _N,        _M_BSPC,   _SPC,      _COM_DOT,  _SL_RALT   \
     ),
 
     [LAYER_LEFT_SYMBOL] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
-        xxx,    xxx,    xxx,    xxx,    xxx,    _RSB,   _PLS,   _AMP,   _LAB,   _RAB,   \
-        _UPPR,  LADJ,   xxx,    xxx,    xxx,    _RPR,   _DQT,   _USD,   _EX,    xxx,    \
+        xxx,    xxx,    xxx,    _TAB,   xxx,    _RSB,   _PLS,   _AMP,   _LAB,   _RAB,   \
+        _UPPR,  LADJ,   xxx,    _ESC,   xxx,    _RPR,   _DQT,   _USD,   _EX,    xxx,    \
         xxx,    xxx,    xxx,    xxx,    xxx,    _RCB,   _EQ,    _AST,   _HSH,   _AT     \
     ),
 
     [LAYER_RIGHT_SYMBOL] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
-        _GBP,   _BTK,   _PIP,   _UND,   _LSB,   xxx,    xxx,    xxx,    xxx,    xxx,    \
-        xxx,    _PRC,   _SQT,   _MIN,   _LPR,   xxx,    xxx,    xxx,    RADJ,   _UPPR,  \
+        _GBP,   _BTK,   _PIP,   _UND,   _LSB,   xxx,    _DEL,   xxx,    xxx,    xxx,    \
+        xxx,    _PRC,   _SQT,   _MIN,   _LPR,   xxx,    _ENT,   xxx,    RADJ,   _UPPR,  \
         _EUR,   _CRT,   _BSL,   _TLD,   _LCB,   xxx,    xxx,    xxx,    xxx,    xxx     \
     ),
 
@@ -92,13 +88,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-void keyboard_post_init_user(void){
-    debug_enable = true;
-    //debug_matrix = true;
-    //debug_keyboard = true;
-    //debug_mouse = true;
-}
-
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case _F_LARR:
@@ -106,10 +95,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             return 180;
 
         /* Important symbol layer keys get short tapping term */
-        case _R_TAB:    case _U_DEL:
         case _D_LSYM:   case _K_RSYM:
-        case _C_BSPC:   case _V_ESC:
-        case _M_ENT:    case _SPC_BSPC:
+        case _V_BSPC:   case _M_BSPC:
         case _COM_DOT:
             return 120;
 
@@ -121,15 +108,15 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-//static bool caps_on = false; /* Tracks state of CAPSLOCK */
-//static bool caps_word_on = false; /* Tracks whether caps word is active */
+static bool caps_on = false; /* Tracks state of CAPSLOCK */
+static bool caps_word_on = false; /* Tracks whether caps word is active */
 
 bool is_pressed(keyrecord_t *record) { return record->event.pressed; }
 bool is_released(keyrecord_t *record) { return !is_pressed(record); }
 bool is_tapped(keyrecord_t *record) { return record->tap.count; }
 bool is_held(keyrecord_t *record) { return !is_tapped(record); }
 
-/*
+
 void process_capslock(uint16_t keycode, keyrecord_t *record){
     #ifdef CONSOLE_ENABLE
         uprintf("0x%04X,%u,%u,%u,%b,0x%02X,0x%02X,%u\n",
@@ -163,10 +150,10 @@ void process_capslock(uint16_t keycode, keyrecord_t *record){
             break;
 
         // Keep capslock on for the following keys
-        case _Q_LGUI: case _P_RGUI: case _A_LALT: case _S_LCTL: case _D_LSYM:
-        case _F_SFT:  case _J_SFT:  case _K_RSYM: case _L_RCTL: case _C_ARR:
-        case _R_LCTL: case _S_LSYM: case _T_SFT:  case _N_SFT:  case _E_RSYM:
-        case _I_RCTL: case _O_RALT:
+        case _Q_LGUI: case _P_RGUI:
+        case _A_LCTL: case _S_LSFT: case _D_LSYM: case _F_LARR:
+        case _J_RNUM: case _K_RSYM: case _L_RSFT:
+        case _Z_LALT: case _V_BSPC: case _M_BSPC:
         case _A: case _B: case _C: case _D: case _E: case _F: case _G: case _H:
         case _I: case _J: case _K: case _L: case _M: case _N: case _O: case _P:
         case _Q: case _R: case _S: case _T: case _U: case _V: case _W: case _X:
@@ -177,9 +164,7 @@ void process_capslock(uint16_t keycode, keyrecord_t *record){
             break;
 
         // Don't disable capslock when symbol keys are held for layers/mods
-        case _SCN_RALT:
-        case _SPC_NUM:
-        case _SCN_RGUI:
+        case _SCN_RCTL:
             if(is_tapped(record) && is_pressed(record)) {
                 if(caps_word_on){
                     tap_code16(_CAPS);
@@ -200,25 +185,13 @@ void process_capslock(uint16_t keycode, keyrecord_t *record){
             }
     }
 }
-*/
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     /* Custom handlers that don't stop key processing */
-    //process_capslock(keycode, record);
-    //process_persistent_layer(keycode, record);
+    process_capslock(keycode, record);
 
     switch(keycode) {
-        case _C_BSPC:
-        case _SPC_BSPC:
-            if(is_held(record)){
-                if(is_pressed(record)) {
-                    register_code16(_BSPC);
-                } else {
-                    unregister_code16(_BSPC);
-                }
-                return false;
-            }
-            break;
         case _COM_DOT:
             if(is_held(record)){
                 if(is_pressed(record)) {
@@ -229,42 +202,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             break;
-        case _R_TAB:
+
+        case _V_BSPC:
+        case _M_BSPC:
             if(is_held(record)){
                 if(is_pressed(record)) {
-                    register_code16(_TAB);
+                    register_code16(_BSPC);
                 } else {
-                    unregister_code16(_TAB);
-                }
-                return false;
-            }
-            break;
-        case _U_DEL:
-            if(is_held(record)){
-                if(is_pressed(record)) {
-                    register_code16(_DEL);
-                } else {
-                    unregister_code16(_DEL);
-                }
-                return false;
-            }
-            break;
-        case _V_ESC:
-            if(is_held(record)){
-                if(is_pressed(record)) {
-                    register_code16(_ESC);
-                } else {
-                    unregister_code16(_ESC);
-                }
-                return false;
-            }
-            break;
-        case _M_ENT:
-            if(is_held(record)){
-                if(is_pressed(record)) {
-                    register_code16(_ENT);
-                } else {
-                    unregister_code16(_ENT);
+                    unregister_code16(_BSPC);
                 }
                 return false;
             }
