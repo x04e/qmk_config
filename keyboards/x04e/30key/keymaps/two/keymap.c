00aded5f8c7de607af79114ecb1d7a9314be1130
Liam Hilliard <liam.h.-@hotmail.com>
2022-11-12 21:46:48 +0000

    Remove old ergo keymaps

--- keyboards/x04e/30key/keymaps/two/keymap.c
+++ keyboards/x04e/30key/keymaps/two/keymap.c
@@ -14,88 +14,88 @@ enum layers {
 #define RADJ      MO(LAYER_RIGHT_ADJUST)
 
 /* Custom QWERTY codes */
-#define _Q_HM     LGUI_T(_Q)
-#define _P_HM     LGUI_T(_P)
+#define _Q_M      LGUI_T(_Q)
+#define _P_M      LGUI_T(_P)
 /* --- */
-#define _A_HM     LSFT_T(_A)
-#define _S_HM     LCTL_T(_S)
-#define _D_SYM    LT(LAYER_LEFT_SYMBOL, _D)
-#define _F_SPEC   LT(LAYER_LEFT_ARROW, _F)
-#define _J_SPEC   LT(LAYER_RIGHT_NUMBER, _J)
-#define _K_SYM    LT(LAYER_RIGHT_SYMBOL, _K)
-#define _L_HM     LCTL_T(_L)
-#define _SCN_HM   LSFT_T(_SCN)
+#define _A_M      LALT_T(_A)
+#define _S_M      LCTL_T(_S)
+#define _D_M      LT(LAYER_LEFT_SYMBOL, _D)
+#define _F_M      LSFT_T(_F)
+#define _J_M      LSFT_T(_J)
+#define _K_M      LT(LAYER_RIGHT_SYMBOL, _K)
+#define _L_M      LCTL_T(_L)
+#define _SCN_M    LALT_T(_SCN)
 /* --- */
-#define _Z_HM     LALT_T(_Z)
-#define _SL_HM    LALT_T(_SL)
+#define _C_M      LT(LAYER_LEFT_ARROW, _C)
+#define _SPC_M    LT(LAYER_RIGHT_NUMBER, _SPC)
 
 /* Other mods */
 #define _UPPR     LT(0, KC_NO)
-#define _COM_DOT  LT(0, _DOT)
-#define _SPC_BSP  LT(0, _SPC)
+#define _DOT_M    LT(0, _DOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_QWERTY] = LAYOUT( \
      /* ────────── ────────── ────────── ────────── ──────────|────────── ────────── ────────── ────────── ────────── */
-        _Q_HM,     _W,        _E,        _R,        _T,        _Y,        _U,        _I,        _O,        _P_HM,    \
-        _A_HM,     _S_HM,     _D_SYM,    _F_SPEC,   _G,        _H,        _J_SPEC,   _K_SYM,    _L_HM,     _SCN_HM,  \
-        _Z_HM,     _X,        _C,        _V,        _B,        _N,        _M,        _SPC_BSP,  _COM_DOT,  _SL_HM    \
+        _Q_M,      _W,        _E,        _R,        _T,        _Y,        _U,        _I,        _O,        _P_M,    \
+        _A_M,      _S_M,      _D_M,      _F_M,      _G,        _H,        _J_M,      _K_M,      _L_M,      _SCN_M,  \
+        _Z,        _X,        _C_M,      _V,        _B,        _N,        _M,        _SPC_M,    _DOT_M,    _SL      \
     ),
+
 
     [LAYER_LEFT_SYMBOL] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
         xxx,    xxx,    xxx,    _TAB,   xxx,    _RSB,   _PLS,   _AMP,   _LAB,   _RAB,   \
-        _UPPR,  LADJ,   xxx,    _BSPC,  xxx,    _RPR,   _DQT,   _USD,   _EX,    _CN,   \
-        xxx,    xxx,    xxx,    _ESC,   xxx,    _RCB,   _EQ,    _AST,   _HSH,   _AT     \
+        xxx,    LADJ,   xxx,    _ESC,   _UPPR,  _RPR,   _DQT,   _USD,   _EX,    _BSPC,  \
+        xxx,    xxx,    xxx,    _ENT,   xxx,    _RCB,   _EQ,    _AST,   _HSH,   _AT     \
     ),
 
     [LAYER_RIGHT_SYMBOL] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
-        _GBP,   _BTK,   _PIP,   _UND,   _LSB,   xxx,    _DEL,   xxx,    xxx,    xxx,    \
-        _SCN,   _PRC,   _SQT,   _MIN,   _LPR,   xxx,    xxx,    xxx,    RADJ,   _UPPR,  \
-        _EUR,   _CRT,   _BSL,   _TLD,   _LCB,   xxx,    _ENT,   xxx,    xxx,    xxx     \
+        _GBP,   _BTK,   _PIP,   _UND,   _LSB,   xxx,    xxx,   xxx,    xxx,    xxx,    \
+        _BSPC,  _PRC,   _SQT,   _MIN,   _LPR,   _UPPR,  _ESC,  xxx,    RADJ,   _UPPR,  \
+        _EUR,   _CRT,   _BSL,   _TLD,   _LCB,   xxx,    _DEL,  xxx,    xxx,    xxx     \
     ),
 
     [LAYER_LEFT_ARROW] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
         xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    \
-        _UPPR,  LADJ,   xxx,    xxx,    xxx,    xxx,    _AR_L,  _AR_D,  _AR_U,  _AR_R,  \
+        xxx,    LADJ,   xxx,    xxx,    xxx,    xxx,    _AR_L,  _AR_D,  _AR_U,  _AR_R,  \
         xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    _HOME,  _PG_D,  _PG_U,  _END    \
     ),
 
     [LAYER_RIGHT_ARROW] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
         xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    \
-        _AR_L,  _AR_D,  _AR_U,  _AR_R,  xxx,    xxx,    xxx,    xxx,    RADJ,   _UPPR,  \
+        _AR_L,  _AR_D,  _AR_U,  _AR_R,  xxx,    xxx,    xxx,    xxx,    RADJ,   xxx,    \
         _HOME,  _PG_D,  _PG_U,  _END,   xxx,    xxx,    xxx,    xxx,    xxx,    xxx     \
     ),
 
     [LAYER_LEFT_NUMBER] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
         xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    _7,     _8,     _9,     xxx,    \
-        _UPPR,  LADJ,   xxx,    xxx,    xxx,    xxx,    _4,     _5,     _6,     _0,     \
+        xxx,    LADJ,   xxx,    xxx,    xxx,    xxx,    _4,     _5,     _6,     _0,     \
         xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    _1,     _2,     _3,     xxx     \
     ),
 
     [LAYER_RIGHT_NUMBER] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
         xxx,    _7,     _8,     _9,     xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    \
-        _0,     _4,     _5,     _6,     xxx,    xxx,    xxx,    xxx,    RADJ,   _UPPR,  \
+        _0,     _4,     _5,     _6,     xxx,    xxx,    xxx,    xxx,    RADJ,   xxx,    \
         xxx,    _1,     _2,     _3,     xxx,    xxx,    xxx,    xxx,    xxx,    xxx     \
     ),
 
     [LAYER_LEFT_ADJUST] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
         xxx,    xxx,    xxx,    xxx,    xxx,    _F6,    _F7,    _F8,    _F9,    _F10,   \
-        _UPPR,  TRNS,   xxx,    xxx,    xxx,    _F12,   _V_P,   _V_VD,  _V_VU,  xxx,    \
+        xxx,    TRNS,   xxx,    xxx,    xxx,    _F12,   _V_P,   _V_VD,  _V_VU,  xxx,    \
         RESET,  xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx     \
     ),
 
     [LAYER_RIGHT_ADJUST] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── */
         _F1,    _F2,    _F3,    _F4,    _F5,    xxx,    xxx,    xxx,    xxx,    xxx,    \
-        xxx,    _V_VD,  _V_VU,  _V_P,   _F11,   xxx,    xxx,    xxx,    TRNS,   _UPPR,  \
+        xxx,    _V_VD,  _V_VU,  _V_P,   _F11,   xxx,    xxx,    xxx,    TRNS,   xxx,    \
         xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    RESET   \
     )
 
@@ -103,13 +103,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case _F_SPEC:
-        case _J_SPEC:
+        case _C_M: case _SPC_M:
             return 180;
 
+        //case _SL_M:
+        //    return 130;
+
         /* Important symbol layer keys get short tapping term */
-        case _D_SYM:   case _K_SYM:
-        case _COM_DOT: case _SPC_BSP:
+        case _DOT_M:
+        case _D_M: case _K_M:
             return 120;
 
         case _UPPR:
@@ -162,8 +164,9 @@ void process_capslock(uint16_t keycode, keyrecord_t *record){
             break;
 
         // Keep capslock on for the following keys
-        case _Q_HM:   case _P_HM:   case _A_HM:  case _S_HM: case _D_SYM:
-        case _F_SPEC: case _J_SPEC: case _K_SYM: case _L_HM: case _Z_HM:
+        case _Q_M: case _P_M:
+        case _A_M: case _S_M: case _D_M: case _F_M: case _J_M: case _K_M: case _L_M:
+        case _C_M:
         case _A: case _B: case _C: case _D: case _E: case _F: case _G: case _H:
         case _I: case _J: case _K: case _L: case _M: case _N: case _O: case _P:
         case _Q: case _R: case _S: case _T: case _U: case _V: case _W: case _X:
@@ -174,8 +177,8 @@ void process_capslock(uint16_t keycode, keyrecord_t *record){
             break;
 
         // Don't disable capslock when symbol keys are held for layers/mods
-        case _SCN_HM:
-        case _SPC_BSP:
+        case _SCN_M:
+        case _SPC_M:
             if(is_tapped(record) && is_pressed(record)) {
                 if(caps_word_on){
                     tap_code16(_CAPS);
@@ -203,17 +206,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     process_capslock(keycode, record);
 
     switch(keycode) {
-        case _SPC_BSP:
-            if(is_held(record)){
-                if(is_pressed(record)) {
-                    register_code16(_BSPC);
-                } else {
-                    unregister_code16(_BSPC);
-                }
-                return false;
-            }
-            break;
-        case _COM_DOT:
+
+        case _DOT_M:
             if(is_held(record)){
                 if(is_pressed(record)) {
                     register_code16(_COM);
@@ -223,7 +217,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             break;
+
+        /*
+        case _SL_M:
+            if(is_held(record)){
+                if(is_pressed(record)) {
+                    register_code16(_SL);
+                } else {
+                    unregister_code16(_SL);
+                }
+                return false;
+            }
+            break;
+        */
     }
     return true;
 }
+
 
