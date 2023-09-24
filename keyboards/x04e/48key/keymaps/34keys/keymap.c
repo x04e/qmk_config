c5def36977b3af9a6abdcccefb51a109829208df
Liam Hilliard <liam.h.-@hotmail.com>
2023-09-24 03:03:32 +0100

    Remove caps-word feature

--- keyboards/x04e/48key/keymaps/34keys/keymap.c
+++ keyboards/x04e/48key/keymaps/34keys/keymap.c
@@ -83,9 +83,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-static bool caps_on = false; /* Tracks state of CAPSLOCK */
-static bool caps_word_on = false; /* Tracks whether caps word is active */
-
 bool is_pressed(keyrecord_t *record) { return record->event.pressed; }
 bool is_released(keyrecord_t *record) { return !is_pressed(record); }
 bool is_tapped(keyrecord_t *record) { return record->tap.count; }
@@ -108,71 +105,6 @@ bool tap_hold(keyrecord_t *record, uint16_t tap_keycode, uint16_t hold_keycode){
     return false;
 }
 
-void process_capslock(uint16_t keycode, keyrecord_t *record){
-    #ifdef CONSOLE_ENABLE
-        uprintf("0x%04X,%u,%u,%u,%b,0x%02X,0x%02X,%u\n",
-             keycode,
-             record->event.key.row,
-             record->event.key.col,
-             get_highest_layer(layer_state),
-             record->event.pressed,
-             get_mods(),
-             get_oneshot_mods(),
-             record->tap.count
-         );
-    #endif
-    switch(keycode) {
-
-        case _UPPR:
-            if(is_pressed(record)) {
-                tap_code16(_CAPS);
-                caps_on = !caps_on;
-
-                if(is_tapped(record)) {
-                    //tapped - treated as caps word
-                    caps_word_on = caps_on;
-
-                } else {
-                    //held - treated as caps lock
-                    caps_word_on = false;
-                }
-            }
-            break;
-
-        // Keep capslock on for the following keys
-        case _Q_M: case _P_M:
-        case _A_M: case _S_M: case _D_M: case _K_M: case _L_M:
-        case _A: case _B: case _C: case _D: case _E: case _F: case _G: case _H:
-        case _I: case _J: case _K: case _L: case _M: case _N: case _O: case _P:
-        case _Q: case _R: case _S: case _T: case _U: case _V: case _W: case _X:
-        case _Y: case _Z:
-        case _1: case _2: case _3: case _4: case _5: case _6: case _7: case _8:
-        case _9: case _0:
-        case _UND: case _MIN: case _BSPC: case _DEL:
-            break;
-
-        // Don't disable capslock when symbol keys are held for layers/mods
-        case _SCN_M:
-            if(is_tapped(record) && is_pressed(record)) {
-                if(caps_word_on){
-                    tap_code16(_CAPS);
-                    caps_on = !caps_on;
-                    caps_word_on = false;
-                }
-            }
-            break;
-
-        // Disable capslock for all other key presses
-        default:
-            if(is_pressed(record)) {
-                if(caps_word_on){
-                    tap_code16(_CAPS);
-                    caps_on = !caps_on;
-                    caps_word_on = false;
-                }
-            }
-    }
-}
 
 //static uint16_t oneshot_timer; //static is persisted between function calls
 
