55cbda3f945b1f84f76f3254598a1a20023f9ea5
Liam Hilliard <liam.h.-@hotmail.com>
2023-09-24 15:43:46 +0100

    Restore default 34key keymap. Move latest 34key to 34keyv2

--- keyboards/x04e/48key/keymaps/34keysv2/keymap.c
+++ keyboards/x04e/48key/keymaps/34keysv2/keymap.c
@@ -2,35 +2,28 @@
 #include <print.h>
 #include <shortcuts.h>
 
-bool is_pressed(keyrecord_t *record);
-bool is_released(keyrecord_t *record);
-bool is_tapped(keyrecord_t *record);
-bool is_held(keyrecord_t *record);
-bool tap_hold(keyrecord_t *record, uint16_t tap_keycode, uint16_t hold_keycode);
-void process_capslock(uint16_t keycode, keyrecord_t *record);
-
-enum layers { QWERTY, NUMARR, SYMBOL, ADJUST };
+enum layers { QWERTY, LOWER, RAISE, ADJUST };
 
 /* --- Row 1 --- */
 #define _Q_M      LGUI_T(_Q)
 #define _P_M      LGUI_T(_P)
 /* --- Row 2 --- */
-#define _A_M      LALT_T(_A)
-#define _S_M      LCTL_T(_S)
-#define _L_M      LCTL_T(_L)
-#define _SCN_M    LALT_T(_SCN)
+#define _A_M      LCTL_T(_A)
+#define _S_M      LSFT_T(_S)
+#define _L_M      LSFT_T(_L)
+#define _SCN_M    LCTL_T(_SCN)
 /* --- Row 3 --- */
+#define _Z_M      LALT_T(_Z)
+#define _SL_M     LALT_T(_SL)
 /* --- Row 4 --- */
-#define LTHM1     LCTL_T(_DEL)
-#define LTHM2     LT(NUMARR, _SPC)
-#define RTHM2     LT(SYMBOL, _BSPC)
-#define RTHM1     LSFT_T(_ENT)
+#define THM1      LSFT_T(_DEL)
+#define THM2      LT(LOWER, _SPC)
+#define THM3      LT(RAISE, _BSPC)
+#define THM4      LSFT_T(_ENT)
 /**/
 
 /* Other mods */
 #define ADJ       MO(ADJUST)
-#define _DOT_M    LT(0, _DOT)
-#define _UPPR     LT(0, KC_NO)
 
 #define _BTK_M    LT(0, _BTK)
 #define _AT_M     LT(0, _AT)
@@ -43,32 +36,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
         xxx,    _Q_M,   _W,     _E,     _R,     _T,     _Y,     _U,     _I,     _O,     _P_M,   xxx,    \
         xxx,    _A_M,   _S_M,   _D,     _F,     _G,     _H,     _J,     _K,     _L_M,   _SCN_M, xxx,    \
-        xxx,    _Z,     _X,     _C,     _V,     _B,     _N,     _M,     _COM,   _DOT,   _SL,    xxx,    \
-        xxx,    xxx,    xxx,    xxx,    LTHM1,  LTHM2,  RTHM2,  RTHM1,  xxx,    xxx,    xxx,    xxx     \
+        xxx,    _Z_M,   _X,     _C,     _V,     _B,     _N,     _M,     _COM,   _DOT,   _SL_M,  xxx,    \
+        xxx,    xxx,    xxx,    xxx,    THM1,   THM2,   THM3,   THM4,   xxx,    xxx,    xxx,    xxx     \
     ),
 
-    [SYMBOL] = LAYOUT( \
+    [LOWER] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
-        xxx,    _TAB,   _BTK_M, _PIP,   _UND,   _LSB,   _RSB,   _PLS,   _AMP,   _LAB_M, _BSPC,  xxx,    \
-        xxx,    _ESC,   _EX,    _SQT,   _MIN,   _LPR,   _RPR,   _DQT,   _USD,   _RAB,   xxx,    xxx,    \
-        xxx,    _UPPR,  _AT_M,  _BSL,   _TLD,   _LCB,   _RCB,   _EQ,    _AST,   _HSH_M, _DEL,   xxx,    \
-        xxx,    xxx,    xxx,    xxx,    ___,    ADJ,    ___,    ___,    xxx,    xxx,    xxx,    xxx     \
+        ___,    xxx,    _7,     _8,     _9,     xxx,    _RSB,   _PLS,   _AMP,   _LAB_M, _BSPC,  ___,    \
+        ___,    _0,     _4,     _5,     _6,     xxx,    _RPR,   _DQT,   _USD,   _RAB,   _ENT,   ___,    \
+        ___,    xxx,    _1,     _2,     _3,     xxx,    _RCB,   _EQ,    _AST,   _HSH_M, _DEL,   ___,    \
+        xxx,    xxx,    xxx,    xxx,    TRNS,   TRNS,   ADJ,    ADJ,    xxx,    xxx,    xxx,    xxx     \
     ),
 
-    [NUMARR] = LAYOUT( \
+    [RAISE] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
-        xxx,    xxx,    _7,     _8,     _9,     xxx,   xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    \
-        xxx,    _0,     _4,     _5,     _6,     xxx,   xxx,    _AR_L,  _AR_D,  _AR_U,  _AR_R,  xxx,    \
-        xxx,    xxx,    _1,     _2,     _3,     xxx,   xxx,    _HOME,  _PG_D,  _PG_U,  _END,   xxx,    \
-        xxx,    xxx,    xxx,    xxx,    ___,    ___,   ADJ,    ___,    xxx,    xxx,    xxx,    xxx     \
+        ___,    _TAB,   _BTK_M, _PIP,   _UND,   _LSB,   xxx,    xxx,    xxx,    xxx,    xxx,    ___,    \
+        ___,    _ESC,   _EX,    _SQT,   _MIN,   _LPR,   xxx,    _AR_L,  _AR_D,  _AR_U,  _AR_R,  ___,    \
+        ___,    xxx,    _AT_M,  _BSL,   _TLD,   _LCB,   xxx,    _HOME,  _PG_D,  _PG_U,  _END,   ___,    \
+        xxx,    xxx,    xxx,    xxx,    ADJ,    ADJ,    TRNS,   TRNS,   xxx,    xxx,    xxx,    xxx     \
     ),
 
     [ADJUST] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
-        xxx,    _F1,    _F2,    _F3,    _F4,    _F5,    _F6,    _F7,    _F8,    _F9,    _F10,   xxx,    \
-        xxx,    xxx,    _V_VD,  _V_VU,  _V_P,   _F11,   _F12,   _V_P,   _V_VD,  _V_VU,  xxx,    xxx,    \
-        xxx,    xxx,    xxx,    xxx,    xxx,    RESET,  RESET,  xxx,    xxx,    xxx,    xxx,    xxx,    \
-        xxx,    xxx,    xxx,    xxx,    ___,    ___,    ___,    ___,    xxx,    xxx,    xxx,    xxx     \
+        ___,    _F1,    _F2,    _F3,    _F4,    _F5,    _F6,    _F7,    _F8,    _F9,    _F10,   ___,    \
+        ___,    xxx,    _V_VD,  _V_VU,  _V_P,   _F11,   _F12,   _V_P,   _V_VD,  _V_VU,  xxx,    ___,    \
+        ___,    RESET,  xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    RESET,  ___,    \
+        xxx,    xxx,    xxx,    xxx,    TRNS,   TRNS,   TRNS,   TRNS,   xxx,    xxx,    xxx,    xxx     \
     )
 
 };
@@ -78,36 +71,16 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         /* Important symbol layer keys get short tapping term */
         case _UPPR:
             return 200;
-        case _BTK_M: case _AT_M: case _LAB_M: case _HSH_M:
+
+        case _BTK_M: case _AT_M:
+        case _LAB_M: case _HSH_M:
+        case THM1: case THM2:
+        case THM3: case THM4:
             return 180;
-        case LTHM2: case RTHM2:
-            return 180;
+
         default:
             return TAPPING_TERM;
     }
-}
-
-static bool caps_on = false; /* Tracks state of CAPSLOCK */
-static bool caps_word_on = false; /* Tracks whether caps word is active */
-
-//static uint16_t oneshot_timer; //static is persisted between function calls
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    /* Custom handlers that don't stop key processing */
-    process_capslock(keycode, record);
-
-    // Time out oneshot mods
-    /*if(timer_elapsed(oneshot_timer) > 1500){
-        clear_oneshot_mods();
-    }*/
-
-    switch(keycode) {
-        case _BTK_M: return tap_hold(record, _BTK,_GBP);
-        case _AT_M:  return tap_hold(record, _AT, _CRT);
-        case _LAB_M: return tap_hold(record, _LAB, _PRC);
-        case _HSH_M: return tap_hold(record, _HSH, _EUR);
-    }
-    return true;
 }
 
 bool is_pressed(keyrecord_t *record) { return record->event.pressed; }
@@ -132,67 +105,24 @@ bool tap_hold(keyrecord_t *record, uint16_t tap_keycode, uint16_t hold_keycode){
     return false;
 }
 
-void process_capslock(uint16_t keycode, keyrecord_t *record){
-    #ifdef CONSOLE_ENABLE
-        uprintf("0x%04X,%u,%u,%u,%b,0x%02X,0x%02X,%u\n",
-             keycode,
-             record->event.key.row,
-             record->event.key.col,
-             get_highest_layer(layer_state),
-             record->event.pressed,
-             get_mods(),
-             get_oneshot_mods(),
-             record->tap.count
-         );
-    #endif
+
+//static uint16_t oneshot_timer; //static is persisted between function calls
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    /* Custom handlers that don't stop key processing */
+    process_capslock(keycode, record);
+
+    // Time out oneshot mods
+    /*if(timer_elapsed(oneshot_timer) > 1500){
+        clear_oneshot_mods();
+    }*/
+
     switch(keycode) {
-
-        case _UPPR:
-            if(is_pressed(record)) {
-                tap_code16(_CAPS);
-                caps_on = !caps_on;
-
-                if(is_tapped(record)) {
-                    //tapped - treated as caps word
-                    caps_word_on = caps_on;
-
-                } else {
-                    //held - treated as caps lock
-                    caps_word_on = false;
-                }
-            }
-            break;
-
-        // Keep capslock on for the following keys
-        case _Q_M: case _P_M: case _A_M: case _S_M: case _L_M:
-        case _A: case _B: case _C: case _D: case _E: case _F: case _G: case _H:
-        case _I: case _J: case _K: case _L: case _M: case _N: case _O: case _P:
-        case _Q: case _R: case _S: case _T: case _U: case _V: case _W: case _X:
-        case _Y: case _Z:
-        case _1: case _2: case _3: case _4: case _5: case _6: case _7: case _8:
-        case _9: case _0:
-        case _UND: case _MIN: case _BSPC: case _DEL:
-            break;
-
-        // Don't disable capslock when symbol keys are held for layers/mods
-        case _SCN_M:
-            if(is_tapped(record) && is_pressed(record)) {
-                if(caps_word_on){
-                    tap_code16(_CAPS);
-                    caps_on = !caps_on;
-                    caps_word_on = false;
-                }
-            }
-            break;
-
-        // Disable capslock for all other key presses
-        default:
-            if(is_pressed(record)) {
-                if(caps_word_on){
-                    tap_code16(_CAPS);
-                    caps_on = !caps_on;
-                    caps_word_on = false;
-                }
-            }
+        case _BTK_M: return tap_hold(record, _BTK, _GBP);
+        case _AT_M:  return tap_hold(record, _AT,  _CRT);
+        case _LAB_M: return tap_hold(record, _LAB, _PRC);
+        case _HSH_M: return tap_hold(record, _HSH, _EUR);
     }
+    return true;
 }
+
