a1c0ded1f9e4a2472b0b7d9cd8d60b4e254e661a
Liam Hilliard <liam.h.-@hotmail.com>
2023-12-10 21:05:21 +0000

    Remove extra thumb buttons

--- keyboards/x04e/48key/keymaps/34keysv3/keymap.c
+++ keyboards/x04e/48key/keymaps/34keysv3/keymap.c
@@ -15,13 +15,9 @@ enum layers { QWERTY, LOWER, RAISE, ADJUST };
 #define _L_M      LCTL_T(_L)
 #define _SCN_M    LALT_T(_SCN)
 /* --- Row 3 --- */
-#define _Z_M      LSFT_T(_Z)
-#define _SL_M     LSFT_T(_SL)
 /* --- Row 4 --- */
-#define THM1      LSFT_T(_SFT)
-#define THM2      LT(LOWER, _SPC)
-#define THM3      MO(RAISE)
-#define THM4      _SFT
+#define LWR      LT(LOWER, _SPC)
+#define RSE      MO(RAISE)
 /**/
 
 /* Other mods */
@@ -37,22 +33,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
         xxx,    _Q_M,   _W,     _E,     _R,     _T,     _Y,     _U,     _I,     _O,     _P_M,   xxx,    \
         xxx,    _A_M,   _S_M,   _D_M,   _F,     _G,     _H,     _J,     _K_M,   _L_M,   _SCN_M, xxx,    \
-        xxx,    _Z_M,   _X,     _C,     _V,     _B,     _N,     _M,     _COM,   _DOT,   _SL_M,  xxx,    \
-        xxx,    xxx,    xxx,    xxx,    THM1,   THM2,   THM3,   THM4,   xxx,    xxx,    xxx,    xxx     \
+        xxx,    _Z,     _X,     _C,     _V,     _B,     _N,     _M,     _COM,   _DOT,   _SL,    xxx,    \
+        xxx,    xxx,    xxx,    xxx,    _SFT,   LWR,    RSE,    _SFT,   xxx,    xxx,    xxx,    xxx     \
     ),
 
     [LOWER] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
-        xxx,    _TAB,   _BTK_M, _PIP,   _UND,   _LSB,   _BSPC,  xxx,    xxx,    xxx,    xxx,    xxx,    \
-        xxx,    _ESC,   _EX,    _SQT,   _MIN,   _LPR,   _ENT,   _AR_L,  _AR_D,  _AR_U,  _AR_R,  xxx,    \
-        xxx,    xxx,    _AT_M,  _BSL,   _TLD,   _LCB,   _DEL,   _HOME,  _PG_D,  _PG_U,  _END,   xxx,    \
+        xxx,    _TAB,   _BTK_M, _PIP,   _UND,   _LSB,   xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    \
+        xxx,    _ESC,   _EX,    _SQT,   _MIN,   _LPR,   xxx,    _AR_L,  _AR_D,  _AR_U,  _AR_R,  xxx,    \
+        xxx,    xxx,    _AT_M,  _BSL,   _TLD,   _LCB,   xxx,    _HOME,  _PG_D,  _PG_U,  _END,   xxx,    \
         xxx,    xxx,    xxx,    xxx,    TRNS,   TRNS,   ADJ,    ADJ,    xxx,    xxx,    xxx,    xxx     \
     ),
 
     [RAISE] = LAYOUT( \
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
-        xxx,    xxx,    _7,     _8,     _9,     _TAB,   _RSB,   _PLS,   _AMP,   _LAB_M, _BSPC,  xxx,    \
-        xxx,    _0,     _4,     _5,     _6,     _ESC,   _RPR,   _DQT,   _USD,   _RAB,   _ENT,   xxx,    \
+        xxx,    xxx,    _7,     _8,     _9,     xxx,    _RSB,   _PLS,   _AMP,   _LAB_M, _BSPC,  xxx,    \
+        xxx,    _0,     _4,     _5,     _6,     xxx,    _RPR,   _DQT,   _USD,   _RAB,   _ENT,   xxx,    \
         xxx,    xxx,    _1,     _2,     _3,     xxx,    _RCB,   _EQ,    _AST,   _HSH_M, _DEL,   xxx,    \
         xxx,    xxx,    xxx,    xxx,    ADJ,    ADJ,    TRNS,   TRNS,   xxx,    xxx,    xxx,    xxx     \
     ),
@@ -61,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* ─────── ─────── ─────── ─────── ─────── ───────|─────── ─────── ─────── ─────── ─────── ─────── */
         xxx,    _F1,    _F2,    _F3,    _F4,    _F5,    _F6,    _F7,    _F8,    _F9,    _F10,   xxx,    \
         xxx,    xxx,    _V_VD,  _V_VU,  _V_P,   _F11,   _F12,   _V_P,   _V_VD,  _V_VU,  xxx,    xxx,    \
-        xxx,    xxx,    xxx,    xxx,    xxx,    RESET,   RESET, xxx,    xxx,    xxx,    xxx,    xxx,    \
-        xxx,    xxx,    xxx,    xxx,    TRNS,   TRNS,   TRNS,   TRNS,   xxx,    xxx,    xxx,    xxx     \
+        xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    \
+        xxx,    xxx,    xxx,    xxx,    RESET,  TRNS,   TRNS,   RESET,  xxx,    xxx,    xxx,    xxx     \
     )
 
 };
@@ -72,8 +68,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         /* Important symbol layer keys get short tapping term */
         case _BTK_M: case _AT_M:
         case _LAB_M: case _HSH_M:
-        case THM1: case THM2:
-        case THM3: case THM4:
+        case LWR: case RSE:
             return 180;
 
         default:
