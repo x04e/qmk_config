91b9a13d6b4898c8784b38510a92bd0763432111
Liam Hilliard <liam.h.-@hotmail.com>
2026-07-03 00:00:17 +0100

    Add builds and scripts for compiling/flashing

---
+++ compile.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd "$(dirname $0)"
+
+cp -r keyboards/x04e qmk_firmware/keyboards/
+
+cd qmk_firmware
+
+if ! echo 3 | qmk setup -y; then
+    echo "Failed to setup QMK!"
+    exit 1
+fi
+
+qmk compile -kb x04e/48key -km default -e ALLOW_WARNINGS=yes &&
+    mv *.hex ../builds/
+
+rm -rf "keyboards/x04e"
+
